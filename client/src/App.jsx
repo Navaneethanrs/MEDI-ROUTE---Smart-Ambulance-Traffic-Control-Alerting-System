afe4f025ab49fa1fa17c54efda4653ffb8d74097
NAVANEETHAN RS <navaneethanrs106@gmail.com>
2026-06-17 20:03:02 +0530

    Update driver dashboard route path to /driver/dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/driver" element={<DriverAuth />} />
-        <Route path="/driver-dashboard" element={<DriverDashboard />} />
+        <Route path="/driver/dashboard" element={<DriverDashboard />} />
         <Route path="/patient-admit" element={<PatientAdmit />} />
         <Route path="/hospital" element={<HospitalDashboard />} />
         <Route path="/how" element={<HowItWorks />} />
